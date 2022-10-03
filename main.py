92b51affe66e045aa19b95df347d5dad2796f90f
unknown <negru.nikita05@gmail.com>
2022-10-03 20:05:47 +0300

    classwork5

---
+++ main.py
@@ -0,0 +1,28 @@
+
+"""  Повторение
+
+
+print(a+b+c)
+print(int(a+b+c))
+print(bool(a==b))
+print(str(a+b))
+print(float(a+c))
+
+def Function(a, b):
+    a=5
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a%b)
+Function(3, 4)
+Function(7, 1)
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+for list in range(0, 3):
+    print(list1[5])
+"""
+
+a = [1, 2, 3]
+print(a[2])
+print(a[a[a[0]]])
+
